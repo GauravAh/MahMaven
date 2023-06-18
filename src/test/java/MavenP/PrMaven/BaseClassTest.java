9c87c78c89bf202c8b02623b9355ce6576b9fe41
Gaurav <gauravabsas12345678@gmail.com>
2023-06-18 18:10:50 +0530

    parameter added

--- src/test/java/MavenP/PrMaven/BaseClassTest.java
+++ src/test/java/MavenP/PrMaven/BaseClassTest.java
@@ -4,8 +4,10 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Parameters;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
@@ -14,11 +16,18 @@ public class BaseClassTest {
 	static ThreadLocal<WebDriver> driver = new ThreadLocal<WebDriver>();
 	Logger log;
 	
+	@Parameters("Browser")
 	@BeforeTest
-	public void setBrowser() {
+	public void setBrowser(String lBrowser) {
+		if(lBrowser.equals("chrome")) {
 		log= LogManager.getLogger(BaseClassTest.class.getName());
 		WebDriverManager.chromedriver().setup();
 		driver.set(new ChromeDriver());
+		}
+		else if(lBrowser.equals("firefox")) {
+			WebDriverManager.firefoxdriver().setup();
+			driver.set(new FirefoxDriver());
+		}
 	}
 	
 	public WebDriver getBrowser() {
